eb157f50f32ec76567ea7095f3eca50fa35cdd4e
tym <1772314831@qq.com>
2019-12-08 23:55:40 +0800

    说明

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wordpress-to-jekyll-exporter
-WordPress to Jekyll Exporter
+
+本项目用于将 wordpress 站点导出为 jekyll 站点（post+img，没有主题）
+
+配置文件见：`pkg/config.js`
+
+1. 修正部分 md 不支持的格式
+
+2. 支持自动修正图片路径引用
+
+3. main.js 自定义 Front Matter
--- pkg/config.js
+++ pkg/config.js
@@ -9,8 +9,12 @@ const mysqlInfo = {
   database: 'wordpress'
 }
 
+// 含有siteDoamin的img会被修正
 const siteDoamin = 'www.myshantou.org'
+// 将wordpress的upload下载到本地
+// 提取图片时直接本地读取比较快
 const uploadPath = path.join(__dirname, `../out/${siteDoamin}/uploads`)
+// 你打算把img放到jekyll的那个目录中
 const jekyllImgBaseUrl = '/images'
 
 // 导出目录
